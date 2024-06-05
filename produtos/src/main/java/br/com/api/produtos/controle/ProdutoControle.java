9106e07a4d5f546f00105ca0ae6f257c65677407
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-05 20:11:00 -0300

    Rota para listar produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,13 +1,23 @@
 package br.com.api.produtos.controle;
 
 import org.springframework.web.bind.annotation.RestController;
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class ProdutoControle {
     
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
+    }
+
     @GetMapping("/")
     public String rota() {
         return "API de produtos funcionando!";

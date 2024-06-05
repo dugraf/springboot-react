cc0b50d82cd46c90e6926f3d8407d7ad92936c50
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-05 20:04:22 -0300

    Configurando repositório

--- produtos/src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
+++ produtos/src/main/java/br/com/api/produtos/repositorio/ProdutoRepositorio.java
@@ -1,5 +1,9 @@
 package br.com.api.produtos.repositorio;
 
-public interface ProdutoRepositorio {
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+
+public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
     
 }

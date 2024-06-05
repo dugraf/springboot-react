bb8a4e2caed82f9aa116e47d9bdd83074331c82d
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-05 20:22:05 -0300

    Rota para cadastrar produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -12,6 +15,11 @@ public class ProdutoControle {
     
     @Autowired
     private ProdutoServico ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {
+        return ps.cadastrar(pm);
+    }
 
     @GetMapping("/listar")
     public Iterable<ProdutoModelo> listar() {

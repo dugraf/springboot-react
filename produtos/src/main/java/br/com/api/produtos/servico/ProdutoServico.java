b892c61a8bcbebd1fbb6b4445c42188e27e37383
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-05 20:16:14 -0300

    Serviço para cadastrar produtos

--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,9 +1,11 @@
 package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -11,7 +13,24 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
+
     public Iterable<ProdutoModelo> listar() {
         return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+        if(pm.getNome().equals("")) {
+            rm.setMensagem("O nome do produto eh obrigatorio!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }
+        else if(pm.getMarca().equals("")) {
+            rm.setMensagem("O nome da marca eh obrigatorio!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }
+        else {
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+        }
     }
 }

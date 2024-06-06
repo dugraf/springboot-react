c2492c01211e790ef99b0c20a60fc06aa874bab1
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-06 13:34:10 -0300

    Obtendo os produtos

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
 
+  //UseState
   const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retono_convertido => setProdutos(retono_convertido));
+  }, [] /*garante a requisicao uma vez*/);
 
   return (
     <div>
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela />
     </div>

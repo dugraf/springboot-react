a984755a22ad4f5c5e3cc8172ac1e31518b804d1
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-06 12:38:38 -0300

    Rota para alterar produtos

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -15,6 +16,11 @@ public class ProdutoControle {
     
     @Autowired
     private ProdutoServico ps;
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm) {

80be6685f28a3bc634e0c8c0b75982fec65d11ac
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-06 19:14:56 -0300

    Objeto produto

--- front/src/App.js
+++ front/src/App.js
@@ -6,6 +6,13 @@ import Tabela from './Tabela';
 
 function App() {
 
+  // Objeto produto
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: ''
+  }
+
   //UseState
   const [btnCadastrar, setBtnCadastrar] = useState(true);
   const [produtos, setProdutos] = useState([]);

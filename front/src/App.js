a14cd29cf2a45aa0e04ebacd100f440355dfb060
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-06 13:24:45 -0300

    Visiblidade dos botões

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+
   return (
     <div>
-      <Formulario />
+      <Formulario botao={btnCadastrar}/>
       <Tabela />
     </div>
   );

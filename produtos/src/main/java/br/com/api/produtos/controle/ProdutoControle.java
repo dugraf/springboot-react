96a14f5366204fd5f3960384aef11f092bd8138b
Eduardo Rodrigues Graf <eduardor.graf@hotmail.com>
2024-06-06 12:45:26 -0300

    CORS

--- produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -6,6 +6,7 @@ import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
     
     @Autowired
